884496d666b4aaad3d84a6bbf10b8ed438087737
CesarR2005 <cramirez10@hawk.iit.edu>
2025-05-01 21:30:34 -0500

    media querie update

--- js/script.js
+++
@@ -1,8 +0,0 @@
-// Get the hamburger button and the nav links
-const hamburger = document.getElementById('hamburger');
-const navLinks = document.getElementById('nav-links');
-
-// Toggle the show class on the nav links when clicked
-hamburger.addEventListener('click', () => {
-  navLinks.classList.toggle('show');
-});
