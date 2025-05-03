eb9fec0187faf0c34a3a6954db33523486206859
dontai cantrell jr <98494230+poiut23@users.noreply.github.com>
2025-05-03 14:41:47 -0500

    Add active class to input on click and improve button hover effects

---
+++ js/script.js
@@ -0,0 +1,18 @@
+var main = function() {
+    
+    $('input').on('click', (event) => {
+        $(event.currentTarget).addClass('active');
+    });// this will add the class 'active' to the input element when clicked
+
+    // Change input background color on button hover
+    $('button').hover(
+        function() {
+            $('input').css('background-color', 'hsl(56, 71%, 64%)');
+        },
+        function() {
+            $('input').css('background-color', '');
+        }
+    );
+};
+
+$(document).ready(main);
